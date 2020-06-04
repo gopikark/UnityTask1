4d710cfd572459628cbf82369b07619e5383066c
gopikark <41663181+gopikark@users.noreply.github.com>
2020-06-04 10:33:17 +0530

    Added files to repo

---
+++ intrernship2/Assets/drawline.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawline : MonoBehaviour
+{
+    private LineRenderer lr;
+    private float counter;
+    private float dist;
+
+    public Transform origin;
+    public Transform dest;
+
+    public float lineDrawSpeed = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.SetPosition(0, origin.position);
+        lr.SetWidth(.45f, .45f);
+
+        dist = Vector3.Distance(origin.position, dest.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(counter<dist)
+        {
+            counter += .1f/lineDrawSpeed;
+            float x = Mathf.Lerp(0, dist, counter);
+            Vector3 pa = origin.position;
+            Vector3 pb = dest.position;
+
+            Vector3 pall = x * Vector3.Normalize(pb - pa) + pa;
+            lr.SetPosition(1, pall);
+        }
+    }
+}
